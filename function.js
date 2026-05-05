f4f4f434bd49bcecf3250a6228cfd5636382c582
shisirthakuri <shahishisir087@gmail.com>
2026-05-05 21:05:11 +0530

    function and loops

---
+++ function.js
@@ -0,0 +1,58 @@
+// console.log("1+1")
+
+
+
+//Regular functions
+// function add(a,b) {
+//     console.log(a+b)
+// }
+// add(5,4)
+
+
+
+//named functions
+
+// let add = function (a, b, c) {
+//     console.log(a + b + c)
+// }
+
+// add(4, 5, 6)
+
+
+//arrow functions
+
+
+// let add = (a, b, c) => {
+//     console.log(a + b + c)
+// }
+// add(4, 5, 6)
+
+// function square(a) {
+//     console.log(a * a)
+// }
+// square(10)
+
+
+//Loops in javascriopt
+//for(initialization; condition ; iteration)
+
+// for (i = 0; i < 3; i++) {
+//     console.log(i)
+// }
+
+
+// const friends = ['shisir', 'kamal', 'kamala']
+// for (i = 0; i < friends.length; i++) {
+//     console.log(friends[i])
+// }
+
+//while loop
+
+// let i = 0;
+// while (i < friends.length) {
+//     console.log(friends[i])
+//     i++;
+// }
+
+console.log(name)
+var name = "shisir shahi"
