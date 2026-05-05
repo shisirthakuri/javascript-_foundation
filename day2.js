f571340a4f9083f9914ed868d1d05e6ef35450fb
shisirthakuri <shahishisir087@gmail.com>
2026-05-05 20:05:33 +0530

    this is the second day

---
+++ day2.js
@@ -0,0 +1,28 @@
+
+//this is the push method
+// const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+// days.push("saturday")
+// console.log(days)
+
+//this is the pop method
+
+// const days = ["monday", "tuesday", "wednesday"]
+// days.pop("")
+// console.log(days)
+
+//this is the unshift method
+
+
+// const days = ["monday", "tuesday", "wednesday"]
+// days.unshift("saturday")
+// console.log(days)
+
+//this is the shift method
+
+// const days = ["monday", "tuesday", "wednesday"]
+// days.shift()
+// console.log(days)
+
+const days = ["monday", "tuesday", "wednesday"]
+console.log(days.length)
+
